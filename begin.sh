f21eff4afb1cd4def4d1831ce9ec7cd64b27458e
jalusw <jalusatrianiwijaya1@gmail.com>
2025-06-21 00:46:43 +0800

    add arch linux

--- begin.sh
+++ begin.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 
-if [ $(uname -n) == "pop-os" ]; then
+OS_TYPE=$(uname -n)
+
+if [ $OS_TYPE == "pop-os" ]; then
+    echo "Setting up for Pop_OS"
+    echo "Updating and upgrading packages"
     sudo add-apt-repository ppa:neovim-ppa/unstable 
+
+    echo "Installing packages"
     sudo apt-get update && sudo apt-get upgrade -y
+
+    echo "Installing packages"
     sudo apt-get install software-properties-common build-essential vim tmux git \
     curl unzip gnupg ca-certificates lsb-release lynx stow xautolock xss-lock zsh \
     pavucontrol neovim python3 python3-dev python3-pip lua5.1
 
+    echo "Installing Packer for neovim"
     git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+    echo "Installing Oh My Zsh for zsh"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+    echo "Cloning dotfiles"
     git clone https://github.com/jalusw/dotfiles.git dotfiles && cd dotfiles
 
+    echo "Stowing dotfiles"
     stow .
 fi
+
+if [ $OS_TYPE == "arch" ]; then
+    echo "Setting up for Arch Linux"
+    echo "Updating and upgrading packages"
+    sudo pacman -Syu
+
+    echo "Installing packages"
+    sudo pacman -S vim tmux git curl unzip gnupg ca-certificates lsb-release lynx stow xautolock xss-lock zsh \
+    pavucontrol neovim python3 python3-dev python3-pip lua5.1
+
+    echo "Installing Packer for neovim"
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+    ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+    echo "Installing Oh My Zsh for zsh"
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    echo "Cloning dotfiles"
+    git clone https://github.com/jalusw/dotfiles.git dotfiles && cd dotfiles
+
+    echo "Stowing dotfiles"
+    stow .
+fi
+
+echo "Done"
