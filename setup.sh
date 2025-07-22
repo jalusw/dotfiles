58f5c4bd6cb4785d796216ddb507569278ceb0cd
jalusw <jalusatrianiwijaya1@gmail.com>
2025-07-22 21:11:05 +0800

    bootstrap script & ansible stuff

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+set -e
 
-
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install -y ansible
+ansible-playbook playbook.yml
